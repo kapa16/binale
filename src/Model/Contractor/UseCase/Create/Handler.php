3d200c851a5b152d7c1e29041c349f0c272dda25
Pavel <kapa16@inbox.ru>
2019-08-15 18:07:12 +0500

    add contractor create handler

--- src/Model/Contractor/UseCase/Create/Handler.php
+++ src/Model/Contractor/UseCase/Create/Handler.php
@@ -4,7 +4,27 @@ declare(strict_types=1);
 
 namespace App\Model\Contractor\UseCase\Create;
 
+use App\Model\Contractor\Entity\Contractor\ContractorRepository;
+use App\Model\Flusher;
+
 class Handler
 {
+    private $contractor;
+    private $flusher;
 
+    /**
+     * Handler constructor.
+     * @param $contractor
+     * @param $flusher
+     */
+    public function __construct(ContractorRepository $contractor, Flusher $flusher)
+    {
+        $this->contractor = $contractor;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command)
+    {
+        if ()
+    }
 }
